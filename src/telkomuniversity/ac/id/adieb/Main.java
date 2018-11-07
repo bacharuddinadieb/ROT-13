33730ee22ad1cc75fbf6abe4eb7cb9c81b9332a8
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2018-11-07 19:23:26 +0700

    Koding Finish

--- src/telkomuniversity/ac/id/adieb/Main.java
+++ src/telkomuniversity/ac/id/adieb/Main.java
@@ -3,6 +3,16 @@ package telkomuniversity.ac.id.adieb;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        String s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.print(c + " -> ");
+            if (c >= 'a' && c <= 'm' || c >= 'A' && c <= 'M') {
+                c += 13;
+            } else if (c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z') {
+                c -= 13;
+            }
+            System.out.println(c);
+        }
     }
 }
